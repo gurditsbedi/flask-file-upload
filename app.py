0117b000f986146035098ef2dea8774f52e0a5ca
Gurdit Singh Bedi <gurditsbedi@gmail.com>
2017-06-17 18:17:22 +0530

    First Commit

---
+++ app.py
@@ -0,0 +1,53 @@
+
+import os
+from flask import Flask, request, redirect, url_for, flash, render_template, send_from_directory
+from werkzeug.utils import secure_filename
+
+UPLOAD_FOLDER = './media/'
+ALLOWED_EXTENSIONS = set(['txt','png','jpg','jpeg'])
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['SECRET_KEY'] = '8ctppOEf0j3OxIU8GeQa10zk'
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024 # 16MB limit
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/', methods=['GET','POST'])
+def upload_file():
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            flash('File Uploaded Sucessfully')
+            filename = secure_filename(file.filename)
+            if not os.path.exists(UPLOAD_FOLDER):
+                os.makedirs(UPLOAD_FOLDER)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('upload_file', filename=filename))
+    # <p>{% get_flash_messages() %}</p>
+    return render_template('file_upload.html')
+
+
+@app.route('/u/')
+def uploads():
+    filelist = os.listdir(UPLOAD_FOLDER)
+    print(filelist)
+    return render_template('uploads_list.html', filelist=filelist)
+
+
+@app.route('/u/<filename>')
+def uploaded_file(filename):
+    print(filename)
+    return send_from_directory(app.config['UPLOAD_FOLDER'],
+                               filename)
+
